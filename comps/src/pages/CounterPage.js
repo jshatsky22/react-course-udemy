0c23eac729479f5e8f5f90ab29c3a2e52a265677
Jesse Shatsky <jesseshatsky@gmail.com>
2023-05-17 17:06:10 -0400

    refactored counter page to use reducers (section 18)

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,34 +1,75 @@
+import { produce } from "immer"; // called when calling useReducer()
 import Button from ".././components/Button";
 import { useReducer } from "react";
 import Panel from "../components/Panel";
 
-const reducer = (state, action) => {};
+const INCREMENT_COUNT = "increment-count";
+const SET_VALUE_TO_ADD = "change-value-to-add";
+const DECREMENT_COUNT = "decrement-count";
+const ADD_VALUE_TO_COUNT = "add_value_to_count";
+
+const reducer = (state, action) => {
+  // use a switch statement for the case in which action.type is equal to each of the values
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1; // immer allows me to manipulate state directly
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.valueToAdd + state.count; // keep the logic in the reducer primarily, to avoid needing to duplicate into each event handler
+      state.valueToAdd = 0;
+      return;
+    default:
+      console.warn("Unexpected action type: ", action.type);
+      return;
+  }
+};
 
 function CounterPage({ initialCount }) {
-  /*   const [count, setCount] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0); */
-  const [state, dispatch] = useReducer(reducer, {
+  // const [count, setCount] = useState(initialCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+
+  // call produce(reducer) to use immer
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
 
+  console.log(state);
+
   const increment = () => {
-    /* setCount(count + 1); */
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
 
   const decrement = () => {
-    /*   setCount(count - 1); */
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+    // setCount(count - 1);
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    /* setValueToAdd(value); */
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    /*     setCount(count + valueToAdd);
-    setValueToAdd(0); */
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
   };
 
   return (
